ae25269b617b559d39d1bc54b31d2cab37d88c36
Luiz Vieira <luiz22_sp@hotmail.com>
2022-01-25 21:46:36 -0300

    04 menu da aplicação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 
 import { AlertModule } from '../shared/components/alert/alert.module';
 import { LoadingModule } from '../shared/components/loading/loading.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
+import { ShowIfLoggedModule } from '../shared/directives/showIfLogged/show-if-logged.module';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +20,9 @@ import { HeaderComponent } from './header/header.component';
     CommonModule, 
     RouterModule,
     AlertModule,
-    LoadingModule
+    LoadingModule,
+    MenuModule, 
+    ShowIfLoggedModule
   ],
   exports: [
     HeaderComponent, 
--- src/app/shared/directives/showIfLogged/show-if-logged.directive.ts
+++ src/app/shared/directives/showIfLogged/show-if-logged.directive.ts
@@ -5,6 +5,8 @@ import { UserService } from 'src/app/core/user/user.service';
   selector: '[showIfLogged]'
 })
 export class ShowIfLoggedDirective implements OnInit{
+  currentDisplay: string;
+
   constructor(
     private userService: UserService,
     private element: ElementRef<any>,
@@ -12,6 +14,16 @@ export class ShowIfLoggedDirective implements OnInit{
   ){}
 
   ngOnInit(): void {
-    !this.userService.isLogged() && this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+    this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+    this.userService.getUser().subscribe(user => {
+      if(user){
+        this.renderer.setStyle(this.element.nativeElement, 'display', this.currentDisplay);
+      }else{
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
+      }
+    });
+
+    // !this.userService.isLogged() && this.renderer.setStyle(this.element.nativeElement, 'display', 'none');
   }
 }
